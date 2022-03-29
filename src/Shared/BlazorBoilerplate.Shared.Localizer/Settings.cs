7bb0f625d981051e850f554bb574f712560a88dc
YFS11013 <83509926@qq.com>
2022-03-29 22:58:18 +0800

    0329

--- src/Shared/BlazorBoilerplate.Shared.Localizer/Settings.cs
+++ src/Shared/BlazorBoilerplate.Shared.Localizer/Settings.cs
@@ -4,8 +4,8 @@
     {
         public const string NeutralCulture = "en-US";
 
-        public static readonly string[] SupportedCultures = { NeutralCulture, "de-DE", "it-IT", "fa-IR", "pt-PT" };
+        public static readonly string[] SupportedCultures = { NeutralCulture, "de-DE", "it-IT", "fa-IR", "pt-PT", "zh-CN" };
 
-        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", NeutralCulture), ("Deutsch", "de-DE"), ("Italiano", "it-IT"), ("پارسی", "fa-IR"), ("Português", "pt-PT") };
+        public static readonly (string, string)[] SupportedCulturesWithName = new[] { ("English", NeutralCulture), ("Deutsch", "de-DE"), ("Italiano", "it-IT"), ("پارسی", "fa-IR"), ("Português", "pt-PT"), ("中文", "zh-CN") };
     }
 }
